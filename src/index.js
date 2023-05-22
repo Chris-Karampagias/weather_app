01cf77db82aaaf07e7d67c57ef7b0bac7dbc6958
Chris <karampagiaschris@gmail.com>
2023-05-22 11:12:23 +0300

    Finish current weather container structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 async function getWeather() {
   const res = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=c96a9b56719a4aba8d394112231805&q=Greece"
+    "http://api.weatherapi.com/v1/forecast.json?key=c96a9b56719a4aba8d394112231805&q=Greece"
   );
   const data = await res.json();
   console.log(data);
